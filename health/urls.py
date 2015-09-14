be200963967330a5d0f3d43189a516728a7375aa
John David Back <john@casamatic.com>
2015-09-14 16:07:40 -0400

    You can now check for cache health

--- health/urls.py
+++ health/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HealthView.as_view(), name='health'),
+    url(r'cache/$', views.CacheHealthView.as_view(), name='health-cache'),
 ]
--- health/views.py
+++ health/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core.cache import cache
+
+import string
+import random
 
 
 class HealthView(View):
@@ -7,3 +11,21 @@ class HealthView(View):
     def get(self, request, *args, **kwargs):
 
         return HttpResponse(status=200)
+
+
+class CacheHealthView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        # Create a random cache key to avoid collision
+        cache_key = ''.join(random.choice(string.ascii_letters) for _ in range(100))
+
+        # Set the key in the cache and see if we can pull it out.
+        cache.set(cache_key, 'works')
+
+        # If so, return 200
+        if cache.get(cache_key) == 'works':
+            return HttpResponse(status=200)
+
+        # If not, return 404
+        return HttpResponseNotFound()
