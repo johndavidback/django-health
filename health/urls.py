0bffebe930e5f48967ada0c0a99378c1e43e4229
John David Back <john@casamatic.com>
2015-09-15 10:58:22 -0400

    Adding in a db endpoint

--- health/urls.py
+++ health/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.HealthView.as_view(), name='health'),
     url(r'cache/$', views.CacheHealthView.as_view(), name='health-cache'),
+    url(r'db/$', views.DBHealthView.as_view(), name='health-db'),
 ]
--- health/views.py
+++ health/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
 from django.core.cache import cache
+from django.db import connection
 
 import string
 import random
@@ -21,7 +22,7 @@ class CacheHealthView(View):
         cache_key = ''.join(random.choice(string.ascii_letters) for _ in range(100))
 
         # Set the key in the cache and see if we can pull it out.
-        cache.set(cache_key, 'works')
+        cache.set(cache_key, 'works', 20)
 
         # If so, return 200
         if cache.get(cache_key) == 'works':
@@ -29,3 +30,23 @@ class CacheHealthView(View):
 
         # If not, return 404
         return HttpResponseNotFound()
+
+
+class DBHealthView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        with connection.cursor() as c:
+
+            if connection.vendor == 'postgresql':
+                from psycopg2 import OperationalError
+                try:
+                    c.execute('SELECT id FROM auth_user LIMIT 1')
+                    result = c.fetchone()
+                except OperationalError:
+                    return HttpResponseServerError()
+            else:
+                c.execute('SELECT id FROM auth_user LIMIT 1')
+                result = c.fetchone()
+
+            return HttpResponse(result, status=200)
